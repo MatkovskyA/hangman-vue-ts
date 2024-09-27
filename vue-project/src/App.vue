fad3c9620780607db400704fab821c17e675eb16
MatkovskyA <a.matkovsky14@gmail.com>
2024-09-27 12:05:29 +0300

    Реализована верстка, стили и логика введения символов + валидация букв

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,25 +1,45 @@
-<template>
-  <div id="app">
-    <GameHeader />
-    <div class="game-container">
-      <GameFigureIcon />
-      <GameWrongLetters />
-      <GameTrueLetters />
-    </div>
-
-    <!-- Container for final message -->
-
-
-    <!-- Notification -->
-    <div class="notification-container show">
-      <p>Вы уже вводили этот символ</p>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import GameHeader from './components/GameHeader.vue'
 import GameFigureIcon from './components/GameFigureIcon.vue'
 import GameWrongLetters from './components/GameWrongLetters.vue'
-import GameTrueLetters from './components/GameTrueLetters.vue';
+import GameTrueLetters from './components/GameTrueWord.vue'
+import GamePopup from './components/GamePopup.vue'
+import GameNotification from './components/GameNotification.vue'
+
+const word = ref('василий')
+const letters = ref<String[]>([])
+const correctLetters = computed(() => letters.value.filter(x => word.value.includes(x)))
+const wrongtLetters = computed(() => letters.value.filter(x => !word.value.includes(x)))
+const notification = ref<InstanceType<typeof GameNotification> | null>(null)
+
+window.addEventListener('keydown', ({ key }) => {
+  if(letters.value.includes(key)) {
+    notification.value?.open()
+    setTimeout(() => notification.value?.close(), 2000)
+    return
+  };
+
+  if(/[а-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase())
+  }
+})
 </script>
+
+<template>
+  <div id="app">
+    {{ word }}
+    {{ letters }}
+    {{ correctLetters }}
+    {{ wrongtLetters }}
+    <GameHeader />
+    <div class="game-container">
+      <GameFigureIcon />
+      <GameWrongLetters :wrong-letters="wrongtLetters"/>
+      <GameTrueLetters :word="word" :correct-letters="correctLetters"/>
+    </div>
+
+    <GamePopup v-if="false" />
+    <GameNotification ref="notification"/>
+  </div>
+</template>
