1a46e09e5b0870488105f6d78efa0930bdd5f680
MatkovskyA <a.matkovsky14@gmail.com>
2024-09-27 10:44:31 +0300

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <GameHeader />
+    <div class="game-container">
+      <GameFigureIcon />
+      <GameWrongLetters />
+      <GameTrueLetters />
+    </div>
+
+    <!-- Container for final message -->
+
+
+    <!-- Notification -->
+    <div class="notification-container show">
+      <p>Вы уже вводили этот символ</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GameHeader from './components/GameHeader.vue'
+import GameFigureIcon from './components/GameFigureIcon.vue'
+import GameWrongLetters from './components/GameWrongLetters.vue'
+import GameTrueLetters from './components/GameTrueLetters.vue';
+</script>
